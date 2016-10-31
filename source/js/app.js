82c60a9bfcbf2415516c58c873eb36cf2bf9eba9
Michal <michal@MacBook-Michal.local>
2016-10-31 14:45:12 +0100

    menu done

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+export class MenuExample extends React.Component{
+  constructor(props){
+    super(props)
 
-ReactDOM.render(<h1> HELLO WORLD </h1>, document.getElementById('app'));
+    this.state = {
+      active : 0
+    }
+  }
+
+  clicked(index){
+    this.setState({
+      active: index
+    })
+  }
+
+
+  render(){
+    return (
+            <div>
+              <h1>HEY</h1>
+              <ul>
+              {
+                this.props.items.map((name, index) => {
+                  let style =''
+
+                  if(this.state.active == index){
+                    style = 'focused'
+                  }
+
+                  return <li className={style} onClick={this.clicked.bind(this, index)} key={index} id={index+1}> {name} </li>
+                })
+              }
+              </ul>
+
+              <div>
+                <p>Selected: {this.props.items[this.state.active]}</p>
+              </div>
+            </div>
+        );
+      }
+    }
+
+
+
+
+ReactDOM.render( <MenuExample items={ ['Home', 'Services', 'About', 'Contact us'] }/>, document.getElementById('app'));
